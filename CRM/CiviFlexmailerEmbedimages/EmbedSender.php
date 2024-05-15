01fe4ec8ba8c6bc98230eb5644df3bfa2970fdbb
Bastien Ho <bastien@avecnous.eu>
2024-05-15 16:47:19 +0200

    Fix deprecated curly braces

--- CRM/CiviFlexmailerEmbedimages/EmbedSender.php
+++ CRM/CiviFlexmailerEmbedimages/EmbedSender.php
@@ -163,7 +163,7 @@ class CRM_CiviFlexmailerEmbedimages_EmbedSender extends \Civi\FlexMailer\Listene
       }
 
       // Register 5xx SMTP response code (permanent failure) as bounce.
-      if (isset($code{0}) && $code{0} === '5') {
+      if (isset($code[0]) && $code[0] === '5') {
         return FALSE;
       }
 
